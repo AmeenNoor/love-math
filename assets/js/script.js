117c50d1756c0eeefba4e3e9427ec0611dc616b2
AmeenNoor <>
2023-07-13 22:54:25 +0000

    Fix code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,7 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     } else {
+        num1 = num1 * num2;
         displayDivisionQuestion(num1, num2);
     }
 }
@@ -84,7 +85,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else {
-        return [Math.floor(operand1 / operand2), "division"];
+        return [operand1 / operand2, "division"];
     }
 }
 
@@ -123,7 +124,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
